a3e8c81041c7d62d0aa429729484e54f8108a71f
Muniba Ahmed <mahmed93@berkeley.edu>
2022-08-02 22:38:47 -0700

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,16 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../config/secrets");
+const Users = require("../users/users-model");
+const {
+  checkUserNameUnique,
+  checkUserNameExists,
+  checkUserBody,
+} = require("../middleware/authMiddleware");
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+router.post("/register", (req, res) => {
+  res.end("implement register, please!");
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -29,8 +38,8 @@ router.post('/register', (req, res) => {
   */
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post("/login", (req, res) => {
+  res.end("implement login, please!");
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
