e23f22c71da9249caa37de95a446ece155be45ae
Muniba Ahmed <mahmed93@berkeley.edu>
2022-08-02 22:43:09 -0700

    wip 4

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,6 +1,22 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../config/secrets");
+
 module.exports = (req, res, next) => {
-  next();
-  /*
+  const token = req.headers.authorization;
+  if (token === undefined) {
+    res.status(401).json({ message: "token required" });
+  } else {
+    jwt.verify(token, JWT_SECRET, async (err, decodedToken) => {
+      if (err) {
+        next({ status: 401, message: "token invalid" });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  }
+};
+/*
     IMPLEMENT
 
     1- On valid token in the Authorization header, call next.
@@ -11,4 +27,3 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
-};
